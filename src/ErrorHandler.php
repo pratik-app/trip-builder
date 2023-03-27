e068cc0b734edf1091169e562eaf23d9c220b589
Pratik More <developer@pratikmore.com>
2023-03-27 17:07:21 -0400

    Update 0.0.4
    Error Handler class that will handle errors and display errors in JSON format

---
+++ src/ErrorHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+class ErrorHandler
+{
+    // Creating function that will throw error in JSON format
+
+    public static function handleException(Throwable $exception): void
+    {
+        // Setting https status code to 500 this will reference to server error
+
+        http_response_code(500);
+
+        // This echo will show the error code, error message, error file, and line number of error
+
+        echo json_encode([
+            "code" => $exception->getCode(),
+            "message" => $exception->getMessage(),
+            "file" => $exception->getFile(),
+            "line" => $exception->getLine()
+        ]);
+    }
+}
+?>
