6d31ea5535bde2140b14ea674a08a35997c60b4f
Pratik More <developer@pratikmore.com>
2023-03-27 22:32:54 -0400

    Update 0.0.7
    POST method is working now. Images added for the reference of how to pass the data in postman, which message we will receive and the database image after adding values

--- src/flightGateway.php
+++ src/flightGateway.php
@@ -30,8 +30,32 @@ class flightGateway
         }
 
         // Returning Data
-        
+
         return $data;
+    }
+    
+    // Create function
+
+    public function createFlight(array $data):string
+    {
+        $sql = "INSERT INTO flights (flight_number, airline_code, departure_airport_code, departure_time, arrival_airport_code, arrival_time, price)
+                VALUES (:flightNumber, :airLineCode, :departureAC, :departureTime, :arrivalAC, :arrivalTime, :price)";
+
+        // Calling Prepare Method
+
+        $stmt = $this->con->prepare($sql);
+
+        // Binding Parameters
+
+        $stmt->bindValue(":flightNumber", $data["flightNumber"], PDO::PARAM_INT);
+        $stmt->bindValue(":airLineCode", $data["airlineCode"], PDO::PARAM_STR);
+        $stmt->bindValue(":departureAC", $data["departureAirportCode"], PDO::PARAM_STR);
+        $stmt->bindValue(":departureTime", $data["departureTime"],PDO::PARAM_STR);
+        $stmt->bindValue(":arrivalAC", $data["arrivalAirportCode"], PDO::PARAM_STR);
+        $stmt->bindValue(":arrivalTime", $data["arrivalTime"], PDO::PARAM_STR);
+        $stmt->bindValue(":price", $data["price"], PDO::PARAM_STR);
+        $stmt->execute();
+        return $this->con->lastInsertId();
     }
 }
 ?>
--- src/flightsController.php
+++ src/flightsController.php
@@ -21,17 +21,25 @@ class flightsController
         }
     }
     private function processResourceRequest (string $method, string $flightNumber):void
-    {
-
-    }
+    {}
 
     private function processCollectionRequest(string $method):void
     {
         // Using Switch method for various http methods
-        
+        // Using array to check whether the post request is empty or not
         switch($method){
             case "GET":
                 echo json_encode($this->flightGateway->getAllFlights());
+                break;
+            case "POST":
+                // $data = (array)json_decode(file_get_contents("php://input"),true);
+                $data = $_POST;
+                // Calling the create method from the Gateway
+                $fNumber = $this->flightGateway->createFlight($data);
+                http_response_code(201);
+                echo json_encode([
+                    "message" => "Flight Added",                    
+                ]);
                 break;
         }
     }
