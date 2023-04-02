3f405fedd386b5d407b2bdfdfafe3458966630a0
Pratik More <developer@pratikmore.com>
2023-04-02 18:38:41 -0400

    Update V. 1.0
    Updating code for live server

--- index.php
+++ index.php
@@ -15,41 +15,41 @@ header("Content-type: application/json; charset=UTF-8"); // Setting Header type 
 
 $parts = explode("/", $_SERVER['REQUEST_URI']); // Getting URL
 
-$connection = new connection("pratikmore.com", "u498926327_tripbuilder","u498926327_pmtripbuilder","]/Vstmy=T6");  // Calling Connection class to connect with Database
+$connection = new connection("localhost", "u498926327_tripbuilder","u498926327_pmtripbuilder","]/Vstmy=T6");  // Calling Connection class to connect with Database
 
 // Getting Second part of url since the first part is Application name
 
-if($parts[2] == "trips") 
+if($parts[3] == "trips") 
 {
-    $id = $parts[3] ?? null;
+    $id = $parts[4] ?? null;
     $flightGateway = new flightGateway($connection); //Calling flightGatway with connection variable
     $flightcontroller = new flightsController($flightGateway); // Calling flightController class with gatway variable
     $flightcontroller->processRequest($_SERVER['REQUEST_METHOD'], $id); //sending Method with id in proceess Request function     
 }
-if($parts[2] == "onewaytrip") 
+if($parts[3] == "onewaytrip") 
 {
     $oneWayGateway = new oneWayGateway($connection); //Calling flightGatway with connection variable
     $oneWaycontroller = new oneWayController($oneWayGateway); // Calling flightController class with gatway variable
     $oneWaycontroller->processRequest($_SERVER['REQUEST_METHOD']); //sending Method with id in proceess Request function     
 }
-if($parts[2] == "roundtrip") 
+if($parts[3] == "roundtrip") 
 {
-    $id = $parts[3] ?? null;
+    $id = $parts[4] ?? null;
     $roundTripGateway = new roundTripGateway($connection); //Calling flightGatway with connection variable
     $roundTripcontroller = new roundTripController($roundTripGateway); // Calling flightController class with gatway variable
     $roundTripcontroller->processRequest($_SERVER['REQUEST_METHOD'], $id); //sending Method with id in proceess Request function     
 }
-if($parts[2] == "airlines")
+if($parts[3] == "airlines")
 {
-    $id = $parts[3] ?? null;
+    $id = $parts[4] ?? null;
     $airlineGateway = new airlineGateway($connection); //Calling Airline Gateway with connection variable
     $airlinescontroller = new airlinesController($airlineGateway); // Calling AirlineController class with gatway variable
     $airlinescontroller->processRequest($_SERVER['REQUEST_METHOD'], $id); //sending Method with id in proceess Request function 
 
 }
-if($parts[2] == "airports")
+if($parts[3] == "airports")
 {
-    $id = $parts[3] ?? null;
+    $id = $parts[4] ?? null;
     $airportGateway = new airportGateway($connection); //Calling Airport Gateway with connection variable
     $airportcontroller = new airportController($airportGateway); // Calling Airport class with gatway variable
     $airportcontroller->processRequest($_SERVER['REQUEST_METHOD'], $id); //sending Method with id in proceess Request function 
